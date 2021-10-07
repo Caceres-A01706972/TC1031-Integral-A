34d2660b9886a3932b4874a2b712a9acd2c3308f
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2021-10-07 10:24:08 -0500

    Agrega archivo con tiemposBase

---
+++ tiemposBase.h
@@ -0,0 +1,16 @@
+/*
+Ricardo Andres Caceres Villibord A01706972
+Archivo con tiempos bases
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+float _short_cincuentaLibre[2] = {20.24,22.93};
+float _short_cienLibre[2] = {44.94,50.25};
+float _short_dosLibre[2] = {99.37,110.43};
