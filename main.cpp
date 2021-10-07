aad52373bbaa63092a41d3f27c7213ccd529983f
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2021-10-07 10:24:33 -0500

    Actualiza Main

--- main.cpp
+++ main.cpp
@@ -12,70 +12,95 @@ Integral A
 #include <cstring>
 #include <fstream>
 
-#include "archivo.h"
+#include "algorithms.h"
+#include "tiemposBase.h"
 
 
 using namespace std;
 
-
-
 int main(){
 
-    int resp;
-    int tiempos[8] = {26, 25, 28, 30, 23, 24, 27, 29};
-    int marcas[2]= {23, 25};
-    int n = 8;
-    int aux[8];
+    List<int> h1;
 
-    cout<<"Lista de tiempos de los nadadores: "<<endl;
-        for(int i = 0; i<n; i++){
-            cout<<tiempos[i]<<" ";
-        }
-    cout<<endl;
+    int resp;
 
     while(true){
 
         cout<<"\nMENU"<<endl;
         cout<<"1. Ordenar los Tiempos de mas Rapido a mas Lento"<<endl;
         cout<<"2. Ver si algun Nadador tiene alguna Marca Tecnica"<<endl;
-        cout<<"3. SALIR"<<endl;
-
+        cout<<"3. Agregar tiempos a la lista"<<endl;
+        cout<<"4. Mostrar mi Lista"<<endl;
+        cout<<"5. Actualizar o Borrar uno de los tiempos"<<endl;
+        cout<<"6. SALIR"<<endl;
         cin>>resp;
     
         if(resp == 1){
-            /*int tiempos[8] = {26, 25, 28, 30, 23, 24, 27, 29};
-            int n = 8;*/
-
-            bubbleSort(tiempos, n);
-            cout<<"Sorted array: "<<endl;
-            for(int i = 0; i<n; i++){
-                cout<<tiempos[i]<<" ";
-            }
-            cout<<endl;
-
-            cout<<"Deseas armar el heat?  (1=Si / 0=No)"<<endl;
-            cin>>resp;
-            if(resp==1){
-                laneAssignment(tiempos, aux);
-                cout<<"Lane Assignment: "<<endl;  
-                for(int i = 0; i<n; i++){
-                    cout<<"Lane "<<i+1<<": "<<aux[i]<<"\n";
-                }
-                cout<<endl;
-            } else if(resp == 0){
-                continue;
-            }
-        } else if(resp == 2){
-            bubbleSort(tiempos, n);
             
-            cout<<"La Marca que deseas buscar es de hombre o mujer?  (Hombre=1 / Mujer=0)"<<endl;
-            cin>>resp;
-            if(resp == 1){
-                cout<<busquedaBinaria(tiempos, n, marcas[0])<<endl;
-            } else if(resp == 0){
-                cout<<busquedaBinaria(tiempos, n, marcas[1])<<endl;
+            h1.insertionSort(h1.head);
+
+        } else if(resp == 2){
+
+            int gender;
+            float tiempoBase;
+            std::cout<<"Introduce la distancia de la prueba de la que quieres buscar un tiempo base (50, 100, 200)"<<std::endl;
+            std::cin>>resp;
+            std::cout<<"De hombre o mujer? (1=hombre/0=mujer)"<<std::endl;
+            std::cin>>gender;
+            if (resp==50){
+              tiempoBase = _short_cincuentaLibre[gender]; 
+            } else if(resp==100){
+                tiempoBase = _short_cienLibre[gender];
+            } else if(resp==200){
+                tiempoBase = _short_dosLibre[gender];
             }
+            if(h1.search(tiempoBase) == 1){
+                std::cout<<"Hay un nadador con tiempo base!"<<std::endl;
+            } else if(h1.search(tiempoBase) == -1){
+                std::cout<<"No hay ningun nadador con tiempo base :("<<std::endl;
+            }
+
         } else if(resp == 3){
+
+            bool continua = true;
+            while(continua){
+                std::cout<<"Ingresa el tiempo que deseas agregar: (whole numbers)"<<std::endl;
+                std::cin>>resp;
+                h1.insertion(resp);
+                std::cout<<"Tu lista es la siguiente por el momento: "<<std::endl;
+                std::cout<<h1.toString()<<std::endl;
+                std::cout<<"Deseas continuar agregando tiempos a la lista? (1=Si/0=No)"<<std::endl;
+                std::cin>>resp;
+                if (resp==0){
+                    continua = false;
+                }
+            }
+
+        } else if(resp == 4){
+
+            std::cout<<"Mostrando tu lista"<<std::endl;
+            std::cout<<h1.toString()<<std::endl;
+
+        } else if(resp == 5){
+
+            std::cout<<"Deseas borrar o actualizar algun tiempo? (1=actualizar/0=borrar)"<<std::endl;
+            std::cin>>resp;
+            if(resp == 1){
+                std::cout<<"Que valor deseas reemplazar?"<<std::endl;
+                int oldVal;
+                std::cin>>oldVal;
+                std::cout<<"Cual es el nuevo valor?"<<std::endl;
+                int newVal;
+                std::cin>>newVal;
+                h1.update(h1.search(oldVal), newVal);
+            } else if(resp == 0){
+                std::cout<<"Que valor deseas borrar?"<<std::endl;
+                int oldVal;
+                std::cin>>oldVal;
+                h1.deleteAt(h1.search(oldVal));
+
+            }
+        } else if(resp == 6){
             break;
         }
     }  
